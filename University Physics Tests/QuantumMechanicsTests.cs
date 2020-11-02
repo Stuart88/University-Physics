1b29606ee399cf349832e50634696c64c4df9173
Stuart Aitken <s.aitken88@gmail.com>
2020-11-02 13:03:08 +0000

    Add quantum tunnel class

---
+++ University Physics Tests/QuantumMechanicsTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UniversityPhysics.QuantumMechanics;
+using UniversityPhysics.UnitsAndConstants;
+
+namespace UniversityPhysics_Tests
+{
+    [TestClass]
+    public class QuantumMechanicsTests
+    {
+        [TestMethod]
+        public void TestQuantumTunnel()
+        {
+            // NOTE - this fails due to floating point innacuracy with very small numbers...
+
+            //Taken from University Physics example 40.7
+
+            QuantumTunnel tunnel = new QuantumTunnel(new Energy(5, UniversityPhysics.Enums.EnergyMeasure.eV), new Energy(2, UniversityPhysics.Enums.EnergyMeasure.eV), 1E-9, Constants.Common.M_e);
+
+            Assert.IsTrue(UniversityPhysics.Maths.MathsHelpers.WithinTolerance(tunnel.TunnelProbability, 7.1E-8));
+
+            tunnel.BarrierWidth = 0.5E-9;
+
+            Assert.IsTrue(UniversityPhysics.Maths.MathsHelpers.WithinTolerance(tunnel.TunnelProbability, 5.2E-4));
+        }
+    }
+}
---
+++ University Physics/QuantumMechanics/QuantumTunnel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UniversityPhysics.UnitsAndConstants;
+
+namespace UniversityPhysics.QuantumMechanics
+{
+    public class QuantumTunnel
+    {
+        #region Public Properties
+
+        public Energy BarrierEnergy { get; set; }
+
+        public double BarrierWidth { get; set; }
+
+        private double K => Math.Sqrt(2d * ParticleMass.Kilograms * (this.BarrierEnergy.Joules - ParticleEnergy.Joules)) / Constants.Common.hBar;
+        public Energy ParticleEnergy { get; set; }
+
+        public Mass ParticleMass { get; set; }
+        public double TunnelProbability => G * Math.Exp(-2d * K * BarrierWidth);
+
+        #endregion Public Properties
+
+        #region Private Properties
+
+        private double G => 16 * (ParticleEnergy.Joules / this.BarrierEnergy.Joules) * (1 - ParticleEnergy.Joules / this.BarrierEnergy.Joules);
+
+        #endregion Private Properties
+
+        #region Public Constructors
+
+        public QuantumTunnel(Energy barrierEnergy, Energy particleEnergy, double barrierWidth, Mass particleMass)
+        {
+            BarrierEnergy = barrierEnergy;
+            BarrierWidth = barrierWidth;
+            ParticleEnergy = particleEnergy;
+            ParticleMass = particleMass;
+        }
+
+        #endregion Public Constructors
+    }
+}
