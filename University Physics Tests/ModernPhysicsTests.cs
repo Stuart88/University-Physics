041893f1903e65d9697022bd3552a72acb24c48f
Stuart Aitken <s.aitken88@gmail.com>
2020-11-03 17:01:10 +0000

    Add Compton Scattering class

---
+++ University Physics Tests/ModernPhysicsTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UniversityPhysics.Maths;
+using UniversityPhysics.ModernPhysics;
+
+namespace UniversityPhysics_Tests
+{
+    [TestClass]
+    public class ModernPhysicsTests
+    {
+        [TestMethod]
+        public void TestComptonScatter()
+        {
+            ComptonScatter scatter = new ComptonScatter(0.24E-9);
+
+            var result = scatter.PerformScatter(MathsHelpers.ToRadians(60));
+
+            Assert.IsTrue(MathsHelpers.WithinTolerance(result.ResultantPhotonWavelength, 0.2412E-9));
+            Assert.IsTrue(MathsHelpers.WithinTolerance(result.ResultantPhotonEnergy.ElectronVolts, 5141));
+            Assert.IsTrue(MathsHelpers.WithinTolerance(result.IncidentPhotonEnergy.ElectronVolts, 5167));
+            Assert.IsTrue(MathsHelpers.WithinTolerance(result.ElectronEnergy.ElectronVolts, 26));
+            Assert.IsTrue(MathsHelpers.WithinTolerance(result.ElectronAngle, MathsHelpers.ToRadians(59.7)));
+        }
+    }
+}
---
+++ University Physics/ModernPhysics/ComptonScatter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UniversityPhysics.Maths;
+using UniversityPhysics.UnitsAndConstants;
+
+namespace UniversityPhysics.ModernPhysics
+{
+    public class ComptonScatter
+    {
+        #region Public Properties
+
+        public double IncidentPhotonWavelength { get; set; }
+
+        #endregion Public Properties
+
+        #region Public Constructors
+
+        /// <param name="photonWavelength">The wavelength of the incident photon</param>
+        public ComptonScatter(double photonWavelength)
+        {
+            this.IncidentPhotonWavelength = photonWavelength;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        /// <summary>
+        /// Performs scatter.
+        /// </summary>
+        /// <param name="scatterAngleRadians">If left null, scatter will be to a random angle between 0 and pi</param>
+        /// <returns></returns>
+        public ComptonScatterResult PerformScatter(double? scatterAngleRadians = null)
+        {
+            Random rand = new Random();
+
+            Energy incidentPhotonEnergy = Constants.Common.h * Constants.Common.C / IncidentPhotonWavelength;
+
+            double resultantPhotonAngle = scatterAngleRadians ?? Math.PI * rand.NextDouble();
+            double resultantPhotonWavelength = Constants.Common.h / (Constants.Common.M_e * Constants.Common.C) * (1 - Math.Cos(resultantPhotonAngle)) + IncidentPhotonWavelength;
+
+            double E0 = Constants.Common.h * Constants.Common.C / IncidentPhotonWavelength;
+            double E1 = Constants.Common.h * Constants.Common.C / resultantPhotonWavelength;
+
+            double electronAngle = Math.Atan(E1 * Math.Sin(resultantPhotonAngle) / (E0 - E1 * Math.Cos(resultantPhotonAngle)));
+
+            double electronKE = E0 - E1;
+            double electronV = Math.Sqrt(2 * electronKE / Constants.Common.M_e);
+            double vx = electronV * Math.Cos(electronAngle);
+            double vy = electronV * Math.Sin(electronAngle);
+            Vector electronVelocity = new Vector(vx, vy);
+
+            Energy resultantPhotonEnergy = Constants.Common.h * Constants.Common.C / resultantPhotonWavelength;
+
+            return new ComptonScatterResult
+            {
+                ElectronVelocity = electronVelocity,
+                ElectronAngle = electronAngle,
+                ResultantPhotonAngle = resultantPhotonAngle,
+                ResultantPhotonWavelength = resultantPhotonWavelength,
+                IncidentPhotonEnergy = incidentPhotonEnergy,
+                ResultantPhotonEnergy = resultantPhotonEnergy,
+                ElectronEnergy = incidentPhotonEnergy.Joules - resultantPhotonEnergy.Joules
+            };
+        }
+
+        #endregion Public Methods
+
+        #region Public Classes
+
+        public class ComptonScatterResult
+        {
+            #region Public Properties
+
+            public double ElectronAngle { get; internal set; }
+            public Energy ElectronEnergy { get; internal set; }
+            public Vector ElectronVelocity { get; internal set; }
+            public Energy IncidentPhotonEnergy { get; internal set; }
+            public double ResultantPhotonAngle { get; internal set; }
+            public Energy ResultantPhotonEnergy { get; internal set; }
+            public double ResultantPhotonWavelength { get; internal set; }
+
+            #endregion Public Properties
+        }
+
+        #endregion Public Classes
+    }
+}
